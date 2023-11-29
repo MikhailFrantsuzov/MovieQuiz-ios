b7eadc07b3092f839ee3e888c9957a5ef5b0e3a4
MikhailFrantsuzov <m.francuzov@gmail.com>
2023-11-30 00:22:47 +0100

    Задание спринта №7. 1. Написать unit- и UI-тесты. 2. Произвести рефакторинг кода на архитектуру MVP.

---
+++ MovieQuiz/MovieQuizTests/PresenterTests.swift
@@ -0,0 +1,8 @@
+//
+//  PresenterTests.swift
+//  MovieQuizTests
+//
+//  Created by Mikhail Frantsuzov on 29.11.2023.
+//
+
+import Foundation
---
+++ MovieQuiz/Presentation/MovieQuizViewControllerProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  MovieQuizViewControllerProtocol.swift
+//  MovieQuiz
+//
+//  Created by Mikhail Frantsuzov on 29.11.2023.
+//
+
+import Foundation
