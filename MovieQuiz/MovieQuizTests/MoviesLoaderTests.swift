56ef535adf10777196a932b3cfb4067827f785d5
MikhailFrantsuzov <m.francuzov@gmail.com>
2023-11-29 21:34:46 +0100

    Рефакторинг. Вынести метод "convert" из MoveQuizViewController в Presenter.

---
+++ MovieQuiz/MovieQuizTests/MoviesLoaderTests.swift
@@ -0,0 +1,8 @@
+//
+//  MoviesLoaderTests.swift
+//  MovieQuizTests
+//
+//  Created by Mikhail Frantsuzov on 29.11.2023.
+//
+
+import Foundation
---
+++ MovieQuiz/Presentation/MovieQuizPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  MovieQuizPresenter.swift
+//  MovieQuiz
+//
+//  Created by Mikhail Frantsuzov on 29.11.2023.
+//
+
+import Foundation
